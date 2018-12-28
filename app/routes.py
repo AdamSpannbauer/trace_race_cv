6c04e6f8617d9d2ae828aff92b4266dffee358d3
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-12-28 08:42:31 -0500

    implement cv2 vidcap flask streaming

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
+
+
+def stream_cv2_vidcap(cv2_vidcap):
+    while True:
+        grabbed, raw_frame = cv2_vidcap.read()
+        jpg_bytes = cv2.imencode('.jpg', raw_frame)[1].tostring()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpg_bytes + b'\r\n')
+
 
 app = Flask(__name__)
 
@@ -13,5 +24,12 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(stream_cv2_vidcap(cv2.VideoCapture(0)),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
