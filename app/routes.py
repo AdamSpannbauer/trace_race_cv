2591ec40b1e0d3181ad0ae3e7787d66b07b1aaa4
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-01-02 15:29:37 -0500

    add no cache decorator

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,24 @@
+from functools import wraps, update_wrapper
+from datetime import datetime
 from flask import Flask, render_template
-from flask import request, Response
+from flask import request, Response, make_response
 import cv2
 import numpy as np
 import base64
 from trace_race import TraceRace
+
+
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+
+    return update_wrapper(no_cache, view)
 
 
 app = Flask(__name__)
@@ -21,14 +36,15 @@ def stream_trace_race(data_path):
     flask_trace_race = TraceRace(frame_width=750, data_path=data_path)
 
     while True:
-        raw_frame = decode_frame(b64_img_data)
+        if b64_img_data is not None:
+            raw_frame = decode_frame(b64_img_data)
 
-        display_frame, keypress = flask_trace_race.play_flask(raw_frame, int(keypress))
+            display_frame, keypress = flask_trace_race.play_flask(raw_frame, int(keypress))
 
-        jpg_bytes = cv2.imencode('.jpg', display_frame)[1].tostring()
+            jpg_bytes = cv2.imencode('.jpg', display_frame)[1].tostring()
 
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + jpg_bytes + b'\r\n')
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpg_bytes + b'\r\n')
 
 
 @app.route('/')
@@ -37,6 +53,7 @@ def home():
 
 
 @app.route('/video_feed')
+@nocache
 def video_feed():
     return Response(stream_trace_race(data_path=IMG_PATH),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
@@ -52,6 +69,7 @@ def decode_frame(b64_str):
 
 
 @app.route('/get_frame', methods=['POST'])
+@nocache
 def get_frame():
     global b64_img_data
     img_uri = request.form['img']
